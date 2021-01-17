5182c34a65b8167dcbeed3bc91d6a17cddfcd933
Selmanerdemm <selmanerdem3447@gmail.com>
2021-01-17 18:02:33 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Congratulations new customer Added. : " + customer.Name + " " + customer.Surname);
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Congratulations customer deleted. : " + customer.Name + " " + customer.Surname);
+        }
+
+        public void List(Customer[] customer)
+        {
+            foreach (var list in customer)
+            {
+                Console.WriteLine(list.Name + " " + list.Surname + " :\n " + list.Mail + " \n " + list.PhoneNumber);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Name = "Selman";
+            customer.Surname = "Erdem";
+            customer.Mail = "info12@gmail.com";
+            customer.PhoneNumber = 555555;
+
+            Customer customer1 = new Customer();
+            customer1.Name = "Cemal";
+            customer1.Surname = "Kaya";
+            customer1.Mail = "info@outlook.com";
+            customer1.PhoneNumber = 5555555;
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Filiz";
+            customer2.Surname = "Akın";
+            customer2.Mail = "info22@hotmail.com";
+            customer2.PhoneNumber = 555555;
+
+
+            Customer[] customers = new Customer[] { customer, customer1, customer2 };
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(customer); // Müşteri Ekleme
+
+            customerManager.Delete(customer1); // Müşteri Silme
+
+            customerManager.List(customers); // Müşterileri Listeleme
+            
+
+            
+        }
+    }
+}
